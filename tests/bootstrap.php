ea1cc50ce5d97da59178b696f7597b09a0dc9a05
Marc Würth <ravage@bluewin.ch>
2014-06-27 16:09:26 +0200

    Added a file header DocBlock to all PHP files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Part of the PHPUnit Selenium2 PageObjects library
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Marc Würth <ravage@bluewin.ch>
+ * @link https://github.com/ravage84/phpunit-selenium2-pageobjects
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @package PHPUnit_Selenium2_PageObjects\Tests
+ */
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
